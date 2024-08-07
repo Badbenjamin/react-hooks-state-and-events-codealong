8a675206e25879bc6a830b5769fb88645947f5b2
Ben McCarthy <bmccarthy90@gmail.com>
2024-08-06 21:53:19 -0400

    lifted button state

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Toggle from "./Toggle";
+import { useState } from "react";
 
 function App() {
+  const [isOn, setIsOn] = useState(Array(3).fill(true))
+
+  function handleClick(i) {
+    const whichButton = isOn.slice()
+    whichButton[i] = whichButton[i] ? false : true;
+    setIsOn(whichButton)
+  }
+
+
+
   return (
     <div>
       <h3>Toggle</h3>
-      <Toggle />
+      <Toggle onClick={() => handleClick(0)} isOn={isOn[0]} />
+      <Toggle onClick={() => handleClick(1)} isOn={isOn[1]} />
+      <Toggle onClick={() => handleClick(2)} isOn={isOn[2]} />
     </div>
   );
 }
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Toggle() {
-  return <button>OFF</button>;
+
+function Toggle({ onClick, isOn }) {
+
+  // const color = !isOn ? "red" : "white";
+
+  return <button onClick={onClick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
